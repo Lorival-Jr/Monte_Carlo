ac11bbf444cd2495cb9468b6f433be34ce984560
Lorival-Jr <lorivaljunior0123@gmail.com>
2023-07-21 17:46:21 -0300

    Fazendo algumas alterações na verossimilhança e testando a maximização

--- main.R
+++ main.R
@@ -3,52 +3,6 @@
 
 # 9. Geração de Valores Aleatórios
 
-# ENTENDENDO O CÓDIGO
-my.dexp             <-      function(x, lambda)
-{
-  lambda*exp(-lambda*x) # Criando a função densidade
-}
-
-my.pexp.numerical   <-      function(x, lambda)
-{
-  my.int          <-      function(x, lambda) # Criando uma função pra utilizar no apply
-  { # Ela integra a fdp, recebendo o parametro lambda pra fdp e o lower sendo o intervalo de integração inf = 0 e o sup = x
-    integrate(my.dexp, lambda = lambda, lower = 0, upper = x)$value # é pego do resultado apenas o valor
-  } 
-  sapply(x, FUN=my.int, lambda) # Aplica nos dados a integral da fdp, ou seja a fda
-}
-
-my.qexp.numerical   <-      function(q, lambda) # Função quantil
-{ 
-  f                 <-      function(P, fixed)  # Função pra ser aplicada no unirrot                    
-  {
-    lambda          <-      fixed$lambda        # fixed é uma lista com 2 dimensões
-    q               <-      fixed$q             # A 1° o valor de lambda, e a 2° todos quantis que queremos calcular
-    criterion       <-      q - my.pexp.numerical(P, lambda)   # É o inverso da probabiladade da FDA                     
-    return(criterion)
-  }
-  P                 <-      numeric(length(q)) # Para cada q passado vai achar uma raiz                                     
-  for(i in 1:length(q))
-  {
-    fixed           <-      list(lambda = lambda, q = q[i]) # É a lista que será passado para função f
-    root.p          <-      uniroot(f, lower = 0, upper = 100, fixed = fixed) 
-    # unirrot procura as raizes da função f, com o intervalo de integração de 0 a 100, o fixed é a lista que a f recebe
-    P[i]            <-      root.p$root # pega apenas a raiz pra retornar
-  }
-  return(P)
-}
-
-my.qexp.numerical(0.9, 2)
-my.qexp.numerical(c(0.9, 0.1), 2)
-
-
-my.rexp <- function(n, lambda)
-{
-  u <- runif(n, min = 0, max = 1) # pega um valor aleatorio entre 0 e 1 e repassa ele pra funcao quantil criada
-  rnd.values <- my.qexp(u, lambda) # A função quantil usa a probabilidade gerada e retorna um quantil
-  return(rnd.values)
-}
-
 # Lindley Geométrica ------------------------------------------------------
 
 ### Forma analítica
@@ -99,39 +53,27 @@ rlingley_geom <- function(n, theta, p)
 ## Testando
 
 #install.packages("LindleyPowerSeries")
-library("LindleyPowerSeries")
+
 
 # Pagina 4
 # https://cran.r-project.org/web/packages/LindleyPowerSeries/LindleyPowerSeries.pdf
 help(dlindleygeometric)
 
 dlindley_geom(c(1, 1.5, 4), 1, 0.4)
-dlindleygeometric(c(1, 1.5, 4), 2, 0.4) # lambda = theta e theta = p
-
 
 plindley_geom(c(1, 1.5, 4), 1, 0.4)
-plindleygeometric(c(1,1.5,4), 2, 0.4)
-
 
 qlindley_geom(c(0.3,0.5,0.8), 2, 0.4)
-qlindleygeometric(c(0.3,0.5,0.8), 2, 0.4)
-
 
 rlingley_geom(100, 2, 0.4)
-rlindleygeometric(100, 2, 0.4)
-
-
-par(mfrow = c(1,2))
 
 hist(rlingley_geom(1000, 0.8, 0.4), freq = F)
-hist(rlindleygeometric(1000, 0.8, 0.4), freq = F)
 
+par(mfrow = c(1,2))
 hist(rlingley_geom(1000, 0.2, 0.4), freq = F)
 hist(rgeom(1000, 0.4), freq = F)
 par(mfrow = c(1,1))
 
-hist(rlingley_geom(1000, 0.8, 0.4), freq = F)
-curve(dlindleygeometric(x, 0.8, 0.4), add=T, col = 'green', xlim= c(0.001,13), lwd = 2)
 
 ### Método numérico
 
@@ -184,3 +126,31 @@ plindley_geom(c(1, 1.5, 4), 1, 0.5)
 
 qlindley_geom_num(c(0.3,0.5,0.8), 2, 0.4, 0, 100)
 qlindley_geom(c(0.3,0.5,0.8), 2, 0.4)
+
+
+log_lindley_geometrica <- function(x, par) # par[1] será theta, par[2] é o p
+{  #Restrições: par[1] > 0 e 0 < par[2] < 1
+  
+  
+  # 2*n*log(par[1]) + n*log(1-par[2]) + sum(log(1+x)) + 
+  #   sum(log(exp(-theta[1]*x))) - n*log(par[1] + 1) -
+  #   2*sum(log(1-theta[2]*(1+(theta[1]*x))))
+  
+  n <- length(x)
+  f1 <- 2*n*log(par[1])
+  f2 <- n*log(1-par[2])
+  f3 <- sum(log(1+x))
+  f4 <- sum(-par[1]*x)
+  f5 <- n*log(par[1]+1)
+  f6 <- 2*sum(log(1-par[2]*(1+par[1]*x/(par[1]+1))*exp(-x*par[1])))
+  
+  ll <- f1 + f2 + f3 + f4 - f5 - f6
+  return(ll)
+  
+}
+
+amostra <- rlingley_geom(10000, 5, 0.8)
+
+optim(par = c(1, 0.3), fn = log_lindley_geometrica2, x = amostra, control = list(fnscale = -1))
+#Restrições: par[1] > 0 e 0 < par[2] < 1
+
